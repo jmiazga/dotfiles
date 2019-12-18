d02a37dce012544cf951be5e3ad9a4dd6d58bc20
jmiazga <jmiazga@gmail.com>
2019-12-18 13:54:09 -0500

    added python3 vndly alias

--- aliases.sh
+++ aliases.sh
@@ -6,7 +6,9 @@
 	alias dcc='docker rm $(docker ps -a -q)'
 	alias dci='docker rmi $(docker images -q)'
 	alias dca='dcc; dci'
-
+	alias vndly='cd ~/projects/vndly; workon vndly'
+	alias vndly3='cd ~/projects/vndly3; . venv/bin/activate'
+	alias cleanLogs='find . -name "vndly*.log*" -type f -delete'
 #NPM ALIASES ------------------------------------
 
 #GIT ALIASES ------------------------------------
