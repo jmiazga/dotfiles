145ce1fc798f6d5a35f548c8f87ceb05de3520a9
Jason Miazga <jason.miazga@workday.com>
2022-01-28 10:01:02 -0500

    fixed typos

--- environment.sh
+++ environment.sh
@@ -4,6 +4,6 @@
 	export GOPATH="$HOME/go"
 	export PATH="/usr/local/opt/icu4c/bin:/usr/local/opt/icu4c/sbin:$GOPATH/bin:$PATH"
 	export PATH="/usr/local/opt/postgresql@11/bin:$PATH"
-	export PKG_CONFIG_PTH="$PKG_CONFIG_PATH:/usr/local/opt/icu4c/lib/pkgconfig"
+	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/icu4c/lib/pkgconfig"
 	export LDFLAGS=-L/usr/local/opt/openssl@1.1/lib
-	export CPPFLAGS=-I/usr/local/opt/openssl@1.1/incude
+	export CPPFLAGS=-I/usr/local/opt/openssl@1.1/include
