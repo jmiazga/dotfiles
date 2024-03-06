98c881ca437452ce75984b731652c4c164b7053a
Jason Miazga <jason.miazga@workday.com>
2024-03-06 10:08:52 -0500

    updated for python 12

--- environment.sh
+++ environment.sh
@@ -3,7 +3,7 @@
 	. /usr/local/opt/asdf/libexec/asdf.sh
 	export GOPATH="$HOME/go"
 	export PATH="/usr/local/opt/icu4c/bin:/usr/local/opt/icu4c/sbin:$GOPATH/bin:$PATH"
-	export PATH="/usr/local/opt/postgresql@11/bin:$PATH"
+	export PATH="/usr/local/opt/postgresql@12/bin:$PATH"
 	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/icu4c/lib/pkgconfig"
 	export LDFLAGS=-L/usr/local/opt/openssl@1.1/lib
 	export CPPFLAGS=-I/usr/local/opt/openssl@1.1/include
